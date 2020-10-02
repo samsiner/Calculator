0faf6c372fe858cd6422958ecc2d1b4195149154
wrinkleintime <ssiner2@gmail.com>
2020-10-02 15:23:03 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sam Siner on 10/2/20.
+//  Copyright © 2020 Digital Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
